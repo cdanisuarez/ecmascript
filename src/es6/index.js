c57f784bfbf7decbd53aad2630de7c3e54d9264f
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2020-12-30 15:21:02 -0500

    Multiline, destructuring assigment, spread operator, let and const

--- src/es6/index.js
+++ src/es6/index.js
@@ -25,3 +25,49 @@ console.log(epicPhrase);
 //es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+/* MULTILINE */
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n' + 'Cras eu gravida magna';
+
+//es6
+let lorem2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Cras eu gravida magna`;
+
+console.log(lorem);
+console.log(lorem2);
+
+/* DESTRUCTURING ASSIGMENT */
+let person = {
+    name: 'Daniela',
+    age: 24,
+    country: 'CO'
+};
+
+console.log(person.name, person.age);
+
+//es6
+let {name, age} = person;
+console.log(name, age);
+
+/* SPREAD OPERATOR */
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+/* LET and CONST */
+{
+    var globalVar = 'Global Var';
+}
+
+{
+    let globaLet = 'Global Let';
+    console.log(globaLet); //It is only avaliable in this scope
+}
+
+console.log(globalVar); //Avaliable in a global scrope that's we can see the console.log here
+
+
+const a = 'b';
+a = 'a'; //It is not possible, we cannot change the value of a constant
