4d7ad64d381f308f562ef18302016d14efa36a2b
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2020-12-30 16:06:00 -0500

    Include and exponents

---
+++ src/es7/es7.js
@@ -0,0 +1,15 @@
+/* INCLUDE */
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+    console.log('is here!');
+} else {
+    console.log('is not here!')
+}
+
+/* EXPONENT ** */
+let base = 5;
+let exponent = 2;
+let result = base ** exponent;
+console.log(result);
+
