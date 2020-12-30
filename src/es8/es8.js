faed2d3e9f899518222b018bf37150fd6dabe053
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2020-12-30 17:36:29 -0500

    Async/Await

--- src/es8/es8.js
+++ src/es8/es8.js
@@ -1,8 +1,8 @@
 /* ENTRIES */
 const data = {
-    frontend: 'Daniela',
-    backend: 'Pepito',
-    design: 'Ana',
+	frontend: 'Daniela',
+	backend: 'Pepito',
+	design: 'Ana',
 };
 
 const entries = Object.entries(data);
@@ -11,9 +11,9 @@ console.log(entries.length);
 
 /** VALUES  */
 const data = {
-    frontend: 'Daniela',
-    backend: 'Pepito',
-    design: 'Ana',
+	frontend: 'Daniela',
+	backend: 'Pepito',
+	design: 'Ana',
 };
 
 const values = Object.values(data);
@@ -27,7 +27,33 @@ console.log(string.padEnd(12, ' ----'));
 
 /** TRAILNG COMMAS */
 const data = {
-    frontend: 'Daniela',
-    backend: 'Pepito',
-    design: 'Ana', //Put a comma here in the last attribute/value
+	frontend: 'Daniela',
+	backend: 'Pepito',
+	design: 'Ana', //Put a comma here in the last attribute/value
+};ß
+
+/** ASYNC/AWAIT */
+const helloWorld = (state) => {
+	return new Promise((resolve, reject) => {
+		(state) 
+			? setTimeout(() => resolve('You are a succesfully person ;)'), 3000)
+			: reject(new Error('This an error! ;)'));
+	});
 };
+
+const helloAsync = async (state) => {
+	const helloAwait = await helloWorld(state);
+	console.log(helloAwait);
+};
+
+
+const anotherFunc = async (state) => {
+	try {
+		const hello = await helloWorld(state);
+		console.log(hello);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+anotherFunc(false);
