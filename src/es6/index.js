7c707139100c62a545a92c2af54c82332b4aa5b5
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2020-12-30 15:39:01 -0500

    Named parameters, arrow functions and promises

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,15 +1,15 @@
 /* DEFAULT PARAMS */
 
 function newFunction(name, age, country) {
-    var name = name || 'Daniela';
-    var age = age || '24';
-    var country = country || 'CO';
-    console.log(name, age, country);
+		var name = name || 'Daniela';
+		var age = age || '24';
+		var country = country || 'CO';
+		console.log(name, age, country);
 }
 
 //es6
 function newFunction2(name= 'Daniela', age = '24', country = 'CO') {
-    console.log(name, age, country);
+		console.log(name, age, country);
 }
 
 newFunction2();
@@ -39,9 +39,9 @@ console.log(lorem2);
 
 /* DESTRUCTURING ASSIGMENT */
 let person = {
-    name: 'Daniela',
-    age: 24,
-    country: 'CO'
+		name: 'Daniela',
+		age: 24,
+		country: 'CO'
 };
 
 console.log(person.name, person.age);
@@ -58,12 +58,12 @@ console.log(education);
 
 /* LET and CONST */
 {
-    var globalVar = 'Global Var';
+		var globalVar = 'Global Var';
 }
 
 {
-    let globaLet = 'Global Let';
-    console.log(globaLet); //It is only avaliable in this scope
+		let globaLet = 'Global Let';
+		console.log(globaLet); //It is only avaliable in this scope
 }
 
 console.log(globalVar); //Avaliable in a global scrope that's we can see the console.log here
@@ -71,3 +71,53 @@ console.log(globalVar); //Avaliable in a global scrope that's we can see the con
 
 const a = 'b';
 a = 'a'; //It is not possible, we cannot change the value of a constant
+
+
+/* NAMED PARAMETERS */
+let name = 'Daniela';
+let age = 24;
+
+let obj1 = {name: name, age: age};
+
+//es6
+let obj2 = {name, age};
+
+console.log(obj1);
+console.log(obj2);
+
+/* ARROW FUNCTIONS */
+//Sintaxis reduced and this not vinculable
+const names = [
+		{
+				name: 'Daniela',
+				age: 24
+		},
+		{
+				name: 'Pepito',
+				age: 25
+		}
+];
+
+let listOfNames = names.map(function (item ) { return item.name; });
+let listOfNames2 = names.map(item => item.name);
+
+console.log(listOfNames);
+console.log(listOfNames2);
+
+/* PROMISES */
+
+const helloPromise = (state) => {
+		return new Promise((resolve, reject) => {
+				if (state) {
+						resolve('Hey!');
+				} else {
+						reject('Ups!');
+				}
+		});
+};
+
+helloPromise(false)
+	.then(res => console.log(res))
+	.catch(err => console.log(err));
+
+
